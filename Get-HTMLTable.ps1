ffb0d6e021135b45cbc4ff9d18f414b8e6b4cd84
Michał Malenda <michalmalenda@gmail.com>
2023-04-27 04:12:59 +0200

    up
    TODO fix layout of the out but for now its working in terms of scraping

---
+++ Get-HTMLTable.ps1
@@ -0,0 +1,39 @@
+#scrape the table from html
+$Global:SOURCE="C:\Powershell-Scrape-HTMLTable\example.html"
+$Global:OUTPUT="C:\Powershell-Scrape-HTMLTable\tables.txt"
+$website=Get-Content $SOURCE
+$temp=@{}
+$scrape=@{}
+$i=0
+$j=0
+$collect=$False
+$exception=$False
+foreach($line in $website){
+    if($collect -and $line -match "<table"){
+        $exception=$True
+    }
+    if($line -match "<table"){#this is without > because sometimes there are parameters added to the open table marker
+        $collect=$True
+    }elseif($line -match "</table>" -and !$exception){
+        $i++
+        $j++
+        $temp[$i]=$line
+        $scrape[$j]=$temp
+        $temp=@{}
+        $collect=$False
+    }elseif($line -match "</table>" -and $exception){
+        $exception=$False
+    }
+    if($collect){
+        $i++
+        $temp[$i]=$line
+    }
+}
+
+Out-File $OUTPUT
+for($k=0; $k -le $scrape.Count; $k++){
+    foreach($line in $scrape[$k].Values){
+        $line | Out-File $OUTPUT -Append
+    }
+    #$scrape[$k].Values | Out-File $OUTPUT -Append
+}
