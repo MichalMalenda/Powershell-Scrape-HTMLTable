b5cacabe95c4471abb8518d2c5326e52818c4f4c
Michał Malenda <michalmalenda@gmail.com>
2023-04-28 16:32:03 +0200

    up
    up n working

--- Get-HTMLTable.ps1
+++ Get-HTMLTable.ps1
@@ -1,6 +1,6 @@
 #scrape the table from html
-$Global:SOURCE="C:\Powershell-Scrape-HTMLTable\example.html"
-$Global:OUTPUT="C:\Powershell-Scrape-HTMLTable\tables.txt"
+$Global:SOURCE="C:\temp\example.html"
+$Global:OUTPUT="C:\temp\tables.txt"
 $website=Get-Content $SOURCE
 $temp=@{}
 $scrape=@{}
@@ -12,8 +12,13 @@ foreach($line in $website){
     if($collect -and $line -match "<table"){
         $exception=$True
     }
-    if($line -match "<table"){#this is without > because sometimes there are parameters added to the open table marker
+    if($line -match "<table" -and !$exception){#this is without > because sometimes there are parameters added to the open table marker
         $collect=$True
+    }elseif($line -match "</table>" -and $exception){
+        $exception=$False
+        #$i++
+        #$j++
+        #$temp[$i]=$line
     }elseif($line -match "</table>" -and !$exception){
         $i++
         $j++
@@ -21,8 +26,6 @@ foreach($line in $website){
         $scrape[$j]=$temp
         $temp=@{}
         $collect=$False
-    }elseif($line -match "</table>" -and $exception){
-        $exception=$False
     }
     if($collect){
         $i++
@@ -30,14 +33,18 @@ foreach($line in $website){
     }
 }
 Out-File $OUTPUT
+$x=0
+$a=0
 for($k=0; $k -le $scrape.Count; $k++){
     #foreach($line in $scrape[$k].Values){
     #    $line | Out-File $OUTPUT -Append
     #}
     #$scrape[$k].Values | Out-File $OUTPUT -Append
-    for($x=0;$x -le $scrape[$k].Values.Count; $x++){
+    $a=$a+$scrape[$k].Values.Count
+    for($x=0; $x -le $a; $x++){
         if($scrape[$k] -ne $null) { 
             $scrape[$k][$x] | Out-File $OUTPUT -Append
         }
-    }#$x++
+    }
+    "____________________" | Out-File $OUTPUT  -Append #linebreak between table blocks
 }
